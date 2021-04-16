5326670869ee99830fbca6113238afbea5e87f30
jtan18 <73002651+jtan18@users.noreply.github.com>
2021-04-16 02:52:09 -0400

    fixing bugs

--- javascript.js
+++ javascript.js
@@ -8,12 +8,21 @@ var tol_quest = 0,
     ans = "";
 var component = "";
 var done = false;
+var endpoint = 1;
 
 var dash_template = Handlebars.compile("<div id=board class=f-box-h><div class=show>Welcome {{username}}</div><div class=show>Score : {{score}}</div> <div class=show>Answered : {{qno}}/{{tol_quest}} </div><div class=show>Time : {{m}} min {{s}} s</div></div>");
 var question_template = Handlebars.compile("<div class=quest> Q{{q}}. {{quest}} </div>");
-var msg_correct_temp = Handlebars.compile("<div id=correct class=correct> {{wow}}!! It's Correct </div>");
-var msg_wrong_temp = Handlebars.compile("<div id=wrong class=wrong>Wrong. <div class=explain>{{explain}}</div><button type=button class=btn-primary onclick=update_question()>Next Question</button></div>");
-var result_temp = Handlebars.compile("<div class=f-box-v><h3>Your Score is : {{score}}</h3><h2>{{txt}}</h2></div>");
+var msg_correct_temp = Handlebars.compile("<div id=correct class=correct> {{wow}}! Correct. </div>");
+var msg_wrong_temp = Handlebars.compile("<div id=wrong class=wrong>Incorrect. <div class=explain>{{explain}}</div><button type=button class=btn-primary onclick=update_question()>Next Question</button></div>");
+var result_temp = Handlebars.compile("<div class=f-box-v><h3>Your Score is : {{score}}</h3><h2>{{txt}}</h2><br><button id=res-btn type=button class=btn-primary onclick=reset()>Start again</button></div>");
+
+const reset = () => {
+    console.log("Reseting");
+    score = 0;
+    qno = 0;
+    time = 0;
+    update_question();
+};
 
 const wrong = () => {
     return msg_wrong_temp({ explain: msg });
@@ -25,15 +34,17 @@ const make_option = (id, opt) => {
 }
 
 const correct = () => {
-    let ww = ["Good work", "Nice!", "Awesome"];
+    let ww = ["Great Work", "Nice", "Awesome"];
     let i = (Math.random() * 100) % ww.length;
     return msg_correct_temp({ wow: ww[Math.floor(i)] });
 }
 
-document.querySelector("#get_uname").addEventListener("click", (e) => {
+const select = (endpoint_) => {
     username = document.getElementById("uname").value.trim();
+    console.log("endpoint_ : ", endpoint_);
+    endpoint = endpoint_;
     if (username == "") {
-        alert("Username is required to give quize.")
+        alert("A username is required to give quiz.")
     } else {
         document.getElementById("login").style.display = "none";
         update_dashboard();
@@ -43,7 +54,7 @@ document.querySelector("#get_uname").addEventListener("click", (e) => {
             update_dashboard();
         }, 1000);
     }
-});
+};
 
 document.querySelector("#submit").addEventListener("click", (e) => {
     if (done) return;
@@ -62,26 +73,33 @@ document.querySelector("#submit").addEventListener("click", (e) => {
             }
         }
     }
+    // wrong 
     document.getElementById("remark").innerHTML = wrong();
 });
 
 const update_dashboard = () => {
-    document.getElementById("dash").innerHTML = dash_template({ username: username, score: score, qno: qno, tol_quest: tol_quest, m: Math.round(time / 60), s: time % 60 });
+    document.getElementById("dash").innerHTML = dash_template({ username: username, score: score, qno: qno - 1, tol_quest: tol_quest, m: Math.round(time / 60), s: time % 60 });
 }
 
 const update_question = async() => {
     component = "";
     document.getElementById("remark").innerHTML = "";
-    done = false;
+    document.getElementById("submit").style.display = "none";
     qno += 1;
     if (qno > tol_quest) {
-        console.log("PEr ", score / tol_quest);
         let mm = ((score / tol_quest) > 0.8) ? "Congrats " : "Sorry ";
         document.getElementById("root").innerHTML = result_temp({ score: score, txt: mm + username });
         document.getElementById("submit").style.display = "none";
+        if (score / tol_quest > 0.8)
+            document.getElementById("res-btn").style.display = "none";
         return;
     }
-    var api = "https://my-json-server.typicode.com/jtan18/Assignment3QuizApp/questions/" + qno;
+    var api;
+    if (endpoint == 1)
+        api = "https://my-json-server.typicode.com/jtan18/Assignment3QuizApp/questions/" + qno;
+    else
+        api = "https://my-json-server.typicode.com/jtan18/jtan18.github.io/questions/" + qno;
+
     fetch(api)
         .then(res => {
             res.json().then(data => {
@@ -94,20 +112,22 @@ const update_question = async() => {
                 document.getElementById("root").innerHTML = component;
                 msg = data.Explanation;
                 ans = data.answer;
+                done = false;
+                document.getElementById("submit").style.display = "flex";
             })
         })
         .catch(err => {
             alert("Unable to fetch data from " + api);
         })
 
-    document.getElementById("submit").style.display = "flex";
 };
 
 const init = async() => {
-    fetch("https://my-json-server.typicode.com/jtan18/Assignment3QuizApp/quiz/")
+    fetch("https://my-json-server.typicode.com/deviduttanayak-github/web/quiz")
         .then(res => {
             res.json().then(data => {
                 tol_quest = data.total_questions;
+
             })
         })
         .catch(err => {
